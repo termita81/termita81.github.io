f6dff5d2458bd39ed73a1a1dc6d0cd92add2fb0e
Razvan Popa <termita@gmail.com>
2026-04-07 16:46:45 +0800

    added prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,41 +2,44 @@ const js = require('@eslint/js')
 const globals = require('globals')
 
 module.exports = [
-  js.configs.recommended,
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'script',
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    },
-    rules: {
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-      'no-console': 'off',
-      'semi': ['error', 'never'],
-      'quotes': ['error', 'single', { avoidEscape: true }],
-      'indent': ['error', 'tab'],
-      'eqeqeq': 'error',
-      'curly': 'error',
-      'camelcase': 'warn',
-      'no-var': 'error',
-      'prefer-const': 'warn',
-      'no-alert': 'off'
-    }
-  },
-  {
-    files: ['build.js'],
-    languageOptions: {
-      sourceType: 'script',
-      globals: {
-        ...globals.node
-      }
-    }
-  },
-  {
-    ignores: ['src/apps/fuel/fuel.js', 'src/apps/breath/breath.js', 'docs/**/*']
-  }
+	js.configs.recommended,
+	{
+		files: ['**/*.js'],
+		languageOptions: {
+			ecmaVersion: 2021,
+			sourceType: 'script',
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		},
+		rules: {
+			'no-unused-vars': [
+				'warn',
+				{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+			],
+			'no-console': 'off',
+			semi: ['error', 'never'],
+			quotes: ['error', 'single', { avoidEscape: true }],
+			indent: ['error', 'tab'],
+			eqeqeq: 'error',
+			curly: 'error',
+			camelcase: 'warn',
+			'no-var': 'error',
+			'prefer-const': 'warn',
+			'no-alert': 'off'
+		}
+	},
+	{
+		files: ['build.js'],
+		languageOptions: {
+			sourceType: 'script',
+			globals: {
+				...globals.node
+			}
+		}
+	},
+	{
+		ignores: ['src/apps/fuel/fuel.js', 'src/apps/breath/breath.js', 'docs/**/*']
+	}
 ]
